72e430e75ccc0417fcdfd678dc26cf2045a6adc8
Marc Prud'hommeaux <mwp1@cornell.edu>
2025-07-03 12:22:04 -0400

    Site updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,12 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+//export default defineConfig({});
+
+export default defineConfig({
+  site: "https://tune-out.github.io",
+  //base: "/docs",
+  //trailingSlash: "always",
+});
+
+
